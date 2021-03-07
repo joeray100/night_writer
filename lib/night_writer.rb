d8863cf345f82adc8fcecb7045effd36521e7e69
Joe Ray <jray100.jr@gmail.com>
2021-03-07 14:35:23 -0700

    Add rest of nightwrite and message/braille files

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,10 @@
 input_file = File.open(ARGV[0], "r")
 message = input_file.read
-
+count = input_file.size
 input_file.close
 
+output_file = File.open(ARGV[1], "w")
+braille = output_file.write(message)
+output_file.close
 
- 
+puts "Created '#{ARGV[1]}' containing #{count} characters"
