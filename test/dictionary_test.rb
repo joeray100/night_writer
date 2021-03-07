bc6d2cb3dcb50754c177a44180707e3f4b841b08
Joe Ray <jray100.jr@gmail.com>
2021-03-07 15:09:56 -0700

    Add require to test_helper and create dictionary file/test

---
+++ test/dictionary_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dictionary'
+
+class DictionaryTest < Minitest::Test
+
+  def setup
+    @dictionary = Dictionary.new
+  end
+
+  def test_it_exists
+    assert_instance_of Dictionary, @dictionary
+  end
+
+  def test_it_can_return_hash_value_from_key
+    assert_equal ["0....."], @dictionary.english_to_brail["a"]
+    assert_equal ["0..000"], @dictionary.english_to_brail["z"]
+  end
+
+end
