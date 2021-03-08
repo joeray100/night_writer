12bffa3a66fc02b52b47a37986c53c213b6b8662
Joe Ray <jray100.jr@gmail.com>
2021-03-08 16:25:39 -0700

    Changes made to test helper and new translator2 class methods/tests

--- lib/translator2.rb
+++ lib/translator2.rb
@@ -9,63 +9,19 @@ class Translator2
     @dictionary2 = Dictionary2.new
   end
 
-  def look_it_up
-    @braille.chars.map do |character|
-      @dictionary.braille_to_english[character]
-    end.flatten
-  end
-
-  def line_one
-    one = look_it_up.map do |array|
-      array[0] + array[1]
+  def braille_lookup
+    group_braille_by_six.map do |braille|
+      @dictionary2.braille_to_english[braille.join]
+    end.flatten.join
     end
-    one.join
-  end
 
-  def line_two
-    two = look_it_up.map do |array|
-      array[2] + array[3]
+    def group_braille_by_six
+      @new_braille.split("").each_slice(6).to_a
     end
-    two.join
-  end
 
-  def line_three
-    three = look_it_up.map do |array|
-      array[4] + array[5]
-    end
-    three.join
-  end
-
-  def formatted_output
-    format = []
-    if line_one.size < 3
-      one = line_one.insert(2, "\n")
-      two = line_two.insert(2, "\n")
-      three = line_three.insert(2, "\n")
-      format << one
-      format << two
-      format << three
-      format.join
-    elsif line_one.size < 80
-      one = line_one + "\n"
-      two = line_two + "\n"
-      three = line_three + "\n"
-      format << one
-      format << two
-      format << three
-      format.join
-  elsif line_one.size > 80
-      one = line_one.insert(80, "\n")
-      two = line_two.insert(80, "\n")
-      three = line_three.insert(80, "\n")
-      format << one
-      format << two
-      format << three
-      format << format[0].slice!(81..161) + "\n"
-      format << format[1].slice!(81..161) + "\n"
-      format << format[2].slice!(81..161) + "\n"
-      format.join
-    end
-  end
+  # def braille_output
+  #   @new_braille.chars.map do |braille|
+  # end
+  # end
 
 end
