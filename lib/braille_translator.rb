3714a9094e93e45e0f175e68cba735f5e44f659e
Joe Ray <jray100.jr@gmail.com>
2021-03-08 21:35:34 -0700

    Update braille traslator methods/tests

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -5,27 +5,55 @@ class BrailleTranslator
 
   def initialize(braille)
     @braille = braille
-    @new_braille = braille.delete("\n")
     @dictionary = Dictionary.new
   end
 
-  def braille_lookup
-    group_braille_by_six.map do |braille|
-      @dictionary.braille_to_english[braille.join]
+  def group_braille_by_six
+    @braille.split("").each_slice(6).to_a
+  end
+
+  def lookup
+    braille_to_word.map do |braille|
+      @dictionary.braille_to_english[braille]
     end.flatten.join
+  end
+
+  def braille_lookup
+    braille_to_word.map do |braille|
+      @dictionary.braille_to_english[braille]
+    end.flatten.join
+  end
+
+  def line_one
+    line1 = []
+    line1 << @braille.split("\n").each_slice(1).to_a[0]
+    one = line1.flatten
+  end
+
+  def line_two
+    line2 = []
+    line2 << @braille.split("\n").each_slice(1).to_a[1]
+    two = line2.flatten
+  end
+
+  def line_three
+    line3 = []
+    line3 << @braille.split("\n").each_slice(1).to_a[2]
+    three = line3.flatten
+  end
+
+  def braille_to_word
+    one = line_one.join
+    two = line_two.join
+    three = line_three.join
+    words = []
+      until one.empty?
+      letter = [].join
+      letter << one.slice!(0..1)
+      letter << two.slice!(0..1)
+      letter << three.slice!(0..1)
+      words << letter
     end
-
-    def group_braille_by_six
-      @new_braille.split("").each_slice(6).to_a
-    end
-
-    def conversion_to_english
-      array = []
-      array << braille.slice(0..1)
-      array << braille.slice(23..24)
-      array << braille.slice(46..47)
-      @dictionary.braille_to_english[array.join]
-    end
-
-
+    words
+  end
 end
--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -9,30 +9,60 @@ class BrailleTranslatorTest < Minitest::Test
   end
 
   def test_it_look_up_a_single_english_character_from_dictionary
+    # skip
     translator =  BrailleTranslator.new("0.00..")
 
     expected = "h"
 
-    assert_equal expected, translator.braille_lookup
+    assert_equal expected, translator.lookup
   end
 
-  def test_brail_one_gives_correct_output_for_two_letters
-    translator =  BrailleTranslator.new("0.00..
-0..0..")
-
-    expected = "he"
-
-    assert_equal expected, translator.braille_lookup
-  end
-
-  def test_it_look_up_multiple_braille_charactes_from_dictionary
+  def test_it_can_seperate_line_one_brail
     translator =  BrailleTranslator.new("0.0.0.0.0....00.0.0.00
 00.00.0..0..00.0000..0
 ....0.0.0....00.0.0...")
 
-    expected = ["hellow world"]
+    expected = ["0.0.0.0.0....00.0.0.00"]
 
-    assert_equal expected, translator.braille_lookup
+    assert_equal expected , translator.line_one
+  end
+
+  def test_it_can_seperate_line_two_brail
+    translator =  BrailleTranslator.new("0.0.0.0.0....00.0.0.00
+00.00.0..0..00.0000..0
+....0.0.0....00.0.0...")
+
+    expected = ["00.00.0..0..00.0000..0"]
+
+    assert_equal expected , translator.line_two
+  end
+
+  def test_it_can_seperate_line_three_brail
+    translator =  BrailleTranslator.new("0.0.0.0.0....00.0.0.00
+00.00.0..0..00.0000..0
+....0.0.0....00.0.0...")
+
+    expected = ["....0.0.0....00.0.0..."]
+
+    assert_equal expected , translator.line_three
+  end
+
+  def test_it_can_extract_braille_into_letters_and_put_in_array
+    translator =  BrailleTranslator.new("0.0.0.0.0....00.0.0.00
+00.00.0..0..00.0000..0
+....0.0.0....00.0.0...")
+
+    expected = ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00.", "......", ".000.0", "0..00.", "0.000.", "0.0.0.", "00.0.."]
+
+    assert_equal expected, translator.braille_to_word
+  end
+
+  def test_it_can_trasnlate_braille_array_of_letters_into_english_words
+    translator =  BrailleTranslator.new("0.0.0.0.0....00.0.0.00
+00.00.0..0..00.0000..0
+....0.0.0....00.0.0...")
+
+    assert_equal "hello world", translator.braille_lookup
   end
 
 end
