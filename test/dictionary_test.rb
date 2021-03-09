c72e5abc513341389cdd1c579b30ce6b47b0023a
Joe Ray <jray100.jr@gmail.com>
2021-03-08 19:12:23 -0700

    Combine test for both methods

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,6 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/dictionary'
+require_relative 'test_helper'
 
 class DictionaryTest < Minitest::Test
 
@@ -12,9 +10,14 @@ class DictionaryTest < Minitest::Test
     assert_instance_of Dictionary, @dictionary
   end
 
-  def test_it_can_return_hash_value_from_key
+  def test_it_can_return_hash_value_from_english_key
     assert_equal ["0....."], @dictionary.english_to_braille["a"]
     assert_equal ["0..000"], @dictionary.english_to_braille["z"]
+  end
+
+  def test_it_can_return_hash_value_from_braille_key
+    assert_equal ["k"], @dictionary.braille_to_english["0...0."]
+    assert_equal ["z"], @dictionary.braille_to_english["0..000"]
   end
 
 end
